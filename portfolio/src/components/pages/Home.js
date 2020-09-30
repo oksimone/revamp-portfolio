4a842292a1499fc8eaa9987f8e08044830fe85f2
ksimone <ksworthy@windstream.net>
2020-09-30 00:19:54 -0400

    committing new changes

--- portfolio/src/components/pages/Home.js
+++ portfolio/src/components/pages/Home.js
@@ -1,22 +1,23 @@
 import React from "react";
+import TypeIt from './TypeIt'
 
 function Home() {
   return (
     <div>
-      <div className="parallax"></div>
+     
+      <div className="parallax">
+      <TypeIt/>
+      </div>
       <div class="overlay">
-        <h1>
-          This is the content that will appear on top of the background image.
-        </h1>
-        <p>Isn't it neat?</p>
+        <h1>About me</h1>
       </div>
       <div class="parallax"></div>
 		<div class="overlay">
-			<h1>This is the content that will appear on top of the background image.</h1>
+			<h1>Project Section.</h1>
 			<p>Isn't it neat?</p>
 		</div>
 		<div class="parallax"></div>
-      <h1>Kristians portfolio</h1>
+     
     </div>
   );
 }
