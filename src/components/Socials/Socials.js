3a73e9fac50413c47963ac57229a6109ec215a11
ksimone <ksworthy@windstream.net>
2020-10-08 01:12:36 -0400

    new changes to social.js

--- src/components/Socials/Socials.js
+++ src/components/Socials/Socials.js
@@ -1,39 +1,23 @@
 import React from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon } from 'mdbreact';
+import { MDBContainer, MDBRow, MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol} from 'mdbreact';
 
 const Socials = () => {
 return (
 <MDBContainer>
   <MDBRow>
     <MDBCol>
-      <form>
-        <p className="h4 text-center mb-4">Write to us</p>
-        <label htmlFor="defaultFormContactNameEx" className="grey-text">
-          Your name
-        </label>
-        <input type="text" id="defaultFormContactNameEx" className="form-control" />
-        <br />
-        <label htmlFor="defaultFormContactEmailEx" className="grey-text">
-          Your email
-        </label>
-        <input type="email" id="defaultFormContactEmailEx" className="form-control" />
-        <br />
-        <label htmlFor="defaultFormContactSubjectEx" className="grey-text">
-          Subject
-        </label>
-        <input type="text" id="defaultFormContactSubjectEx" className="form-control" />
-        <br />
-        <label htmlFor="defaultFormContactMessageEx" className="grey-text">
-          Your message
-        </label>
-        <textarea type="text" id="defaultFormContactMessageEx" className="form-control" rows="3" />
-        <div className="text-center mt-4">
-                  <MDBBtn color="warning" outline type="submit">
-                    Send
-                    <MDBIcon far icon="paper-plane" className="ml-2" />
-                  </MDBBtn>
-                </div>
-              </form>
+    <MDBCol>
+      <MDBCard style={{ width: "34rem" }}>
+    
+        <MDBCardBody>
+          <MDBCardTitle>Get Connected with Kristian</MDBCardTitle>
+          <MDBCardText>
+            Click on of the links below to get connected with my social media accounts. OR, click the email button to send me an email.
+          </MDBCardText>
+          <MDBBtn href="#">Email me</MDBBtn>
+        </MDBCardBody>
+      </MDBCard>
+    </MDBCol>
             </MDBCol>
           </MDBRow>
         </MDBContainer>
